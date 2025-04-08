2026b0deb9c7e4cc2edde75d8a706193da0fe79b
DarshanBarde <darshanbarde805@gmail.com>
2025-04-08 18:55:47 +0530

    Admin panel Creation

--- demo/demo/Scripts/BluePrint/AdminDashboard/urls.py
+++ demo/demo/Scripts/BluePrint/AdminDashboard/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
    path('admin_dashboard/', views.dashboard_view, name='admin_dashboard'),
+   path('analytics/', views.analytics_view, name='analytics'),
+   path('user_management/', views.user_management_view, name='user_management'),
+   path('organization/', views.organization_view, name='organization'),
 ]
