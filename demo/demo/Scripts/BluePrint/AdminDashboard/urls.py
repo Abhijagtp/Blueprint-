316a9dee8ccc5d10fb5af096b83264f7104ebfe9
DarshanBarde <darshanbarde805@gmail.com>
2025-04-08 21:57:51 +0530

    updates in admin panel

--- demo/demo/Scripts/BluePrint/AdminDashboard/urls.py
+++ demo/demo/Scripts/BluePrint/AdminDashboard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
    path('analytics/', views.analytics_view, name='analytics'),
    path('user_management/', views.user_management_view, name='user_management'),
    path('organization/', views.organization_view, name='organization'),
+   path('mentor_user/', views.mentor_user_view, name='mentor_user'),
 ]
