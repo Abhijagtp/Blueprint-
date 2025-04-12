b182d244e7f69367e13e000a51b40218e18709d5
Abhishek Jagtap <159562929+Abhijagtp@users.noreply.github.com>
2025-04-12 13:51:00 +0530

    models

--- demo/demo/Scripts/BluePrint/AdminDashboard/models.py
+++ demo/demo/Scripts/BluePrint/AdminDashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class newmodel(models.Model):
+    """
+    Model representing the admin dashboard.
+    """
+    # Define fields for the admin dashboard model here
+    # For example:
+    total_users = models.CharField(max_length=30,default=0)
+    total_posts = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Admin Dashboard - Users: {self.total_users}, Posts: {self.total_posts}, Comments: {self.total_comments}"
--- demo/demo/Scripts/BluePrint/Users/models.py
+++ demo/demo/Scripts/BluePrint/Users/models.py
@@ -345,3 +345,22 @@ class User_form(models.Model):
     
     def __str__(self):
         return f"Profile - {self.current_occupation}"
+    
+
+
+
+
+
+
+class newmodel(models.Model):
+    """
+    Model representing the admin dashboard.
+    """
+    # Define fields for the admin dashboard model here
+    # For example:
+    total_users = models.CharField(max_length=30,default=0)
+    total_posts = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Admin Dashboard - Users: {self.total_users}, Posts: {self.total_posts}, Comments: {self.total_comments}"
